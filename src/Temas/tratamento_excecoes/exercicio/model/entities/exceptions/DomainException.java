391d05fc26dbe4a33beebf53218d0416cc12cd6e
Lucas Franca <lucascfranca11@gmail.com>
2023-01-17 19:02:17 -0300

    resolução de exercicio de exceptions

--- src/Temas/tratamento_excecoes/exercicio/model/entities/exceptions/DomainException.java
+++ src/Temas/tratamento_excecoes/exercicio/model/entities/exceptions/DomainException.java
@@ -1,6 +1,6 @@
 package Temas.tratamento_excecoes.exercicio.model.entities.exceptions;
 
-public class    DomainException extends Exception {
+public class DomainException extends Exception {
 
     // Construtor para receber uma mensagem e assim personalizar a exceção
     public DomainException(String message) {
