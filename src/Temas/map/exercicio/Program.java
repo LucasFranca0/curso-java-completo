77465e12dee72da294321c0ec139cd5571d53d57
Lucas Franca <lucascfranca11@gmail.com>
2023-02-09 20:52:11 -0300

    Set / Map

---
+++ src/Temas/map/exercicio/Program.java
@@ -0,0 +1,47 @@
+package Temas.map.exercicio;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Map<String, Integer> votes = new TreeMap<>();
+
+        System.out.print("Enter file full path: ");
+        String path = scan.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
+
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                }
+                else {
+                    votes.put(name, count);
+                }
+                line = br.readLine();
+            }
+
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        scan.close();
+
+    }
+}
---
+++ src/Temas/set/exercicio/entities/Student.java
@@ -0,0 +1,32 @@
+package Temas.set.exercicio.entities;
+
+import java.util.Objects;
+
+public class Student {
+    private Integer id;
+
+    public Student(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
